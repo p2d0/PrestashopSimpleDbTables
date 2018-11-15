40100b38568e07e900cd488558fae9f762f997e2
Andrew <cerkin-3@yandex.ru>
2018-11-15 16:29:25 +0300

    fixes

--- PrestaTable.php
+++ PrestaTable.php
@@ -1,7 +1,7 @@
 <?php
 namespace SimpleTables;
 use SimpleTables\Db\PrestaDb;
-class PrestaTable extends SimpleTable{
+abstract class PrestaTable extends SimpleTable{
     function __construct(){
         parent::__construct(new PrestaDb());
     }
